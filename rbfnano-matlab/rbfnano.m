4ca2ffe68d1c58acb491f2bb4d1498eceb3b8dca
Matthew Conway <matthew.f.conway@gmail.com>
2014-07-09 10:53:34 -0400

    about to overwrite with apoorv's work

--- rbfnano-matlab/rbfnano.m
+++ rbfnano-matlab/rbfnano.m
@@ -117,12 +117,12 @@ errors = gsubtract(targets,outputs);
 performance = perform(net,targets,outputs)
 
 % Recalculate Training, Validation and Test Performance
-trainTargets = targets .* tr.trainMask{1};
-valTargets = targets  .* tr.valMask{1};
-testTargets = targets  .* tr.testMask{1};
-trainPerformance = perform(net,trainTargets,outputs)
-valPerformance = perform(net,valTargets,outputs)
-testPerformance = perform(net,testTargets,outputs)
+% trainTargets = targets .* tr.trainMask{1};
+% valTargets = targets  .* tr.valMask{1};
+% testTargets = targets  .* tr.testMask{1};
+% trainPerformance = perform(net,trainTargets,outputs)
+% valPerformance = perform(net,valTargets,outputs)
+% testPerformance = perform(net,testTargets,outputs)
 
 % View the Network
 % view(net)
@@ -131,7 +131,7 @@ testPerformance = perform(net,testTargets,outputs)
 % Uncomment these lines to enable various plots.
 % figure, plotperform(tr)
 % figure, plottrainstate(tr)
-% figure, plotconfusion(targets,outputs)
-% figure, plotroc(targets,outputs)
-% figure, ploterrhist(errors)
+ figure, plotconfusion(targets,outputs)
+ figure, plotroc(targets,outputs)
+ figure, ploterrhist(errors)
 
